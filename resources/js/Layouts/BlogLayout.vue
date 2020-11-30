111d417474cafcb60d4ce87c142c2b596e7bb67b
pranayaryal <drpranayaryal@gmail.com>
2020-11-29 22:43:55 -0500

    initial commit

---
+++ resources/js/Layouts/BlogLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen bg-gray-100">
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    components:{
+
+    }
+
+}
+</script>
---
+++ resources/js/Pages/Blog.vue
@@ -0,0 +1,17 @@
+<template>
+    <blog-layout>
+
+    </blog-layout>
+
+</template>
+
+<script>
+import BlogLayout from '@/Layouts/BlogLayout'
+
+export default {
+    components: {
+        BlogLayout
+    }
+
+}
+</script>
